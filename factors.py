88e6dc42db187e87e86e08e626ba8ab1ab17ed49
Mary Ouma <maryawish54@gmail.com>
2022-12-24 02:39:19 -0800

    factors

---
+++ factors.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+#import pdb; pdb.set_trace()
+import math
+import sys
+
+
+filename = sys.argv[1]
+
+with open(filename, 'r', encoding='utf-8') as f:
+    a = f.read()
+    a_1 = a.count("\n")
+lineas = 0
+b = 0
+with open(filename, 'r', encoding='utf-8') as f:
+    while lineas < a_1:
+        n1 = int (f.readline())
+        lineas += 1
+        a = 2
+        while a < n1//2:
+            if math.fmod(n1, a) == 0:
+                print("{:d} = {:d} * {:d}".format(n1, a, int(n1/a), end=""))
+                a = n1
+            a += 1
